72f141db5b449336bc422b7ac750cebf34e2471f
Thomas Ruggeri <truggeri@gmail.com>
2025-11-26 14:03:17 -0800

    feat: Solver with basic techniques

---
+++ app/lib/techniques/only_one_candidate.rb
@@ -0,0 +1,11 @@
+module Techniques
+  class OnlyOneCandidate
+    def self.call(position, puzzle)
+      elem = puzzle.get!(position)
+      return [] if elem.square.solved
+      return [] if elem.square.candidates.length != 1
+
+      [ { type: :solve, position: position, value: elem.square.candidates.first } ]
+    end
+  end
+end
